1528f1d74e614b222af3344b48c888f0b06e4e08
Ali Salmeh <alisalmeh18@gmail.com>
2023-10-25 12:08:03 +0330

    add ClothingDetails class

---
+++ ClothingDetails.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AliSalmeh_ProjectWeek10_InventoryOfProducts
+{
+    public class ClothingDetails
+    {
+        public string Size { get; set; }
+        public string Material { get; set; }
+
+        public ClothingDetails(string size, string material)
+        {
+            Size = size;
+            Material = material;
+        }
+
+        public override string ToString()
+        {
+            return $"Size: {Size}, " + $"Material: {Material}";
+        }
+    }
+}
