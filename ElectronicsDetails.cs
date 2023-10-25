e59ca928d31acfcd2be707f2d103d1c5bb2242e0
Ali Salmeh <alisalmeh18@gmail.com>
2023-10-25 12:08:16 +0330

    add ElectronicsDetails class

---
+++ ElectronicsDetails.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AliSalmeh_ProjectWeek10_InventoryOfProducts
+{
+    public class ElectronicsDetails
+    {
+        public string Brand { get; set; }
+        public double Price { get; set; }
+
+        public ElectronicsDetails(string brand, double price)
+        {
+            Brand = brand;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"Brand: {Brand}, " + $"Price: ${Price}";
+        }
+    }
+}
