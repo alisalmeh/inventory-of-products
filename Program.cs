b961dc701bc90f2e05a1f3ec47321b1d01ec97a2
Ali Salmeh <alisalmeh18@gmail.com>
2023-10-25 12:09:01 +0330

    add Program class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AliSalmeh_ProjectWeek10_InventoryOfProducts
 {
@@ -6,7 +7,22 @@ namespace AliSalmeh_ProjectWeek10_InventoryOfProducts
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create a list of products using generics
+            List<Product<object>> inventory = new List<Product<object>>()
+            {
+                new Product<object>("Laptop", ProductCategory.Electronics, new ElectronicsDetails("Dell", 899.99)),
+                new Product<object>("T-Shirt", ProductCategory.Clothing, new ClothingDetails("Medium", "Cotton")),
+                new Product<object>("Book", ProductCategory.Books,"Introduction to C# Programming"),
+                new Product<object>("Apple", ProductCategory.Food, "Red Delicious")
+            };
+
+            // Display information about all the products in the inventory
+            foreach (var product in inventory)
+            {
+                Console.WriteLine("----------");
+                product.DisplayInfo();
+                Console.WriteLine("----------");
+            }
         }
     }
 }
