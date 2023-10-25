ddbdaee7a5aee945897bcb87ba1cb7c3f3ffddfb
Ali Salmeh <alisalmeh18@gmail.com>
2023-10-25 11:08:52 +0330

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AliSalmeh_ProjectWeek10_InventoryOfProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
